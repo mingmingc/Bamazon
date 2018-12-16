a33ee27921ba9ce6159289fb7e283543dce2eafe
mingmingc <caressi.m@gmail.com>
2018-12-16 14:18:03 -0800

    required mysql, inquirer, connected to server

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,20 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Duckies4$",
+  database: "bamazon"
+});
+
 // Running this Node application will first display all of the items available for sale. 
 // Include the ids, names, and prices of products for sale.
 
