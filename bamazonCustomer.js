c1b87ed2d864067e443083a4b70a48bfbc286ee7
mingmingc <caressi.m@gmail.com>
2018-12-16 14:40:59 -0800

    added prompts for user purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,10 +15,40 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-// Running this Node application will first display all of the items available for sale. 
-// Include the ids, names, and prices of products for sale.
+//connect to mySQL DB
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+});
 
-
+// Running this Node application will first display all of the items available for sale.
+//Include the ids, names, and prices of products for sale. 
+function start() {
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function(err, results) {
+        if (err) throw err;
+        console.log(results);
+        inquirer
+        .prompt([
+            {
+                name: "selectProduct",
+                type: "input", 
+                message: "What would you like to buy? Provide iten ID#:"
+            },
+            {
+                name: "selectQuantity",
+                type: "input", 
+                message: "How many units of product would you like to buy?"
+            }
+        ])
+        // Once the customer has placed the order, your application should check if 
+        // your store has enough of the product to meet the customer's request.
+        .then(function(answer) {
+            console.log(answer.selectProduct); 
+            console.log(answer.selectQuantity);
+            console.log(results[answer.selectProduct - 1].stock_quantity);
+        });
+    });
+}
 
 // The app should then prompt users with two messages.
 // The first should ask them the ID of the product they would like to buy.
@@ -26,8 +56,11 @@ var connection = mysql.createConnection({
 
 
 
-// Once the customer has placed the order, your application should check if 
-// your store has enough of the product to meet the customer's request.
+
+
+
+
+
 
 
 
