0a174861cec19e9a68ee4ecaa9d2ea58bc5e97c1
mingmingc <caressi.m@gmail.com>
2018-12-16 14:14:09 -0800

    created SQL schema, seeds

--- schema.sql
+++ schema.sql
@@ -1,14 +1,18 @@
--- Create a MySQL Database called bamazon.
--- Then create a Table inside of that database called products.
--- The products table should have each of the following columns:
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
 
+USE bamazon;
 
+CREATE TABLE products (
+	item_id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(45) NOT NULL,
+    department_name VARCHAR(15) NOT NULL,
+    price DECIMAL(10,2) default 0,
+    stock_quantity INT default 0,
+    PRIMARY KEY (item_id)
+);
 
--- item_id (unique id for each product)
--- product_name (Name of product)
--- department_name
--- price (cost to customer)
--- stock_quantity (how much of the product is available in stores)
+SELECT * from products;
 
 
 
--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,12 @@
--- Populate this database with around 10 different products. 
--- (i.e. Insert "mock" data rows into this database and table).
-
--- product_name (Name of product)
--- department_name
--- price (cost to customer)
--- stock_quantity (how much of the product is available in stores)
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("1999 honda civic", "auto", 2000.20, 1), 
+("red lipstick", "beauty/health", 10.00, 2),
+("wool gloves", "clothing", 9.99, 5),
+("copic markers", "office/art", 19.99, 3),
+("pashmina scarf", "clothing", 9.99, 20),
+("ring", "jewelry", 299.99, 1),
+("wine glasses", "home", 15.00, 4), 
+("gummy bears", "food", 3.00, 100),
+("painting", "home", 70.00, 1), 
+("oven", "kitchen", 149.99, 12),
+("wrench set", "tools", 9.99, 3); 
