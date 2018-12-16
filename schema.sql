3d6e3ec95cd2c568517abbca1bb233f1a3092af1
mingmingc <caressi.m@gmail.com>
2018-12-16 13:56:49 -0800

    created files

---
+++ schema.sql
@@ -0,0 +1,14 @@
+-- Create a MySQL Database called bamazon.
+-- Then create a Table inside of that database called products.
+-- The products table should have each of the following columns:
+
+
+
+-- item_id (unique id for each product)
+-- product_name (Name of product)
+-- department_name
+-- price (cost to customer)
+-- stock_quantity (how much of the product is available in stores)
+
+
+
---
+++ seeds.sql
@@ -0,0 +1,7 @@
+-- Populate this database with around 10 different products. 
+-- (i.e. Insert "mock" data rows into this database and table).
+
+-- product_name (Name of product)
+-- department_name
+-- price (cost to customer)
+-- stock_quantity (how much of the product is available in stores)
